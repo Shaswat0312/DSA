4cccc430ecad26907c5f777f2e44b77ecba493bf
Shaswat Mishra <mishra3shaswat@gmail.com>
2025-12-06 04:01:29 +0530

    Reverse String

--- ReverseString.java
+++ ReverseString.java
@@ -3,9 +3,9 @@ class Solution {
         
          int i =0,j = s.length - 1;
         while(i < j){
-            char temp = s[j];
-            s[j] = s[i];
-            s[i] = temp;
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
             i++;
             j--;
         }
